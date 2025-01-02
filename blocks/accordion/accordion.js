950e7dbda68d06bd688135b41ba16d9d1de4bc24
rogers <rparedes@costra.ec>
2025-01-02 13:26:52 -0500

    quotecorr

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -9,14 +9,14 @@ export default function decorate(block) {
     // decorate accordion item label
     const label = row.children[0];
     const summary = document.createElement("");
-    summary.className = "accordion-item-label";
+    summary.className = 'accordion-item-label';
     summary.append(...label.childNodes);
     // decorate accordion item body
     const body = row.children[1];
-    body.className = "accordion-item-body";
+    body.className = 'accordion-item-body';
     // decorate accordion item
-    const details = document.createElement("details");
-    details.className = "accordion-item";
+    const details = document.createElement('details');
+    details.className = 'accordion-item';
     details.append(summary, body);
     row.replaceWith(details);
   });
